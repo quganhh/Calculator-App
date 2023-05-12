4b55c157f726e52023f54729d8c7ee9018be556e
quganhh <quanganhdinh156@gmail.com>
2023-05-12 23:37:18 +0700

    Initial Commit

---
+++ Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by Quang Anh on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
